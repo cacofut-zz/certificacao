1509de2a56cebcfc8bb83128aab6e76a7286125d
Cristiano <cacofut@hotmail.com>
2018-10-19 03:21:56 -0300

    Realizado os exercícios do 1 ao 6

---
+++ assessment_test/_C.java
@@ -0,0 +1,18 @@
+public class _C{
+	private static int $;
+	public static void main(String[] main){
+		String a_b;
+		System.out.print($);
+		System.out.print(a_b);	
+	} }
+
+/*
+1.What is the result of the following class? (Choose all that apply)
+A. Compiler error on line 1.
+B. Compiler error on line 2.
+C. Compiler error on line 4.
+D. Compiler error on line 5.
+E. Compiler error on line 6. (certo)
+F. 0null
+G. nullnull
+*/
