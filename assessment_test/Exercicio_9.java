0b187e9f5f83fb40d905c41aa234511d64bb60e6
Cristiano Carvalho Amaral <cacofut@hotmail.com>
2018-10-22 21:53:33 -0300

    atu

---
+++ assessment_test/Exercicio_9.java
@@ -0,0 +1,21 @@
+/*
+9. Which of the following statements are true? (Choose all that apply)
+	A. Checked exceptions are intended to be thrown by the JVM (and not the programmer).
+    A. As exceções verificadas devem ser lançadas pela JVM (e não pelo programador).
+    
+	B. Checked exceptions are required to be caught or declared.
+	B. Exceções verificadas são necessárias para serem capturadas ou declaradas.
+	
+	C. Errors are intended to be thrown by the JVM (and not the programmer).
+	C. Erros são planejados para serem lançados pela JVM (e não pelo programador).
+	
+	D. Errors are required to be caught or declared.
+	D. Erros são necessários para serem capturados ou declarados.
+	
+	E. Runtime exceptions are intended to be thrown by the JVM (and not the programmer).
+	E. Exceções de tempo de execução devem ser lançadas pela JVM (e não pelo programador).
+	
+	F. Runtime exceptions are required to be caught or declared.
+	F. Exceções de tempo de execução precisam ser capturadas ou declaradas.
+	
+*/
